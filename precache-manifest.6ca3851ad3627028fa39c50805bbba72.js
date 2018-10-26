2fad8ea9866737895d9385fe22adcaa2006251f8
Deiver Julio <deiver-jc@live.com>
2018-10-26 15:22:47 -0500

    Updates

---
+++ precache-manifest.6ca3851ad3627028fa39c50805bbba72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e9c2840a93eb0e9e6c4",
+    "url": "/blog-crud/static/js/runtime~main.5e9c2840.js"
+  },
+  {
+    "revision": "33b5a5f7057af2cd94eb",
+    "url": "/blog-crud/static/js/main.33b5a5f7.chunk.js"
+  },
+  {
+    "revision": "05f68e7fd3a16d0ceb47",
+    "url": "/blog-crud/static/js/1.05f68e7f.chunk.js"
+  },
+  {
+    "revision": "33b5a5f7057af2cd94eb",
+    "url": "/blog-crud/static/css/main.18c45120.chunk.css"
+  },
+  {
+    "revision": "05f68e7fd3a16d0ceb47",
+    "url": "/blog-crud/static/css/1.a78d3c71.chunk.css"
+  },
+  {
+    "revision": "5b76087050e8d4b7514f0c5dcd4b954a",
+    "url": "/blog-crud/index.html"
+  }
+];
